0314a2dcf3d326ce9111e06c4b358ac10991de97
Sagar S <59260702+sagar1412@users.noreply.github.com>
2021-03-22 14:42:04 +0530

    Update Sub_file2.py

--- Sub_file2.py
+++ Sub_file2.py
@@ -206,6 +206,7 @@ def app():
     import plotly.figure_factory as ff
     import numpy as np
     import plotly.express as px
+    import plotly
     # Add histogram data
     x1 = np.random.randn(200) - 2
     x2 = np.random.randn(200)
